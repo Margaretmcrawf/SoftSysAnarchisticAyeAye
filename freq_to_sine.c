be23f1b794078c895c539f1b1136bc667602e065
margaretmcrawf <margaretmcrawf@gmail.com>
2017-02-13 11:47:11 -0500

    delay loop for frequency

--- freq_to_sine.c
+++ freq_to_sine.c
@@ -3,11 +3,15 @@
 * (or wave of some sort) at the desired frequency as an array.
 * Currently, it prints 256 values between 0 and 256 that represent a sine wave, 
 * then asks the user for a frequency and prints it back to them.
+
+* Note the extra parameters, which allow math to work.
+
 * Authors: Margo Crawford and Joseph Lee.
 */
 
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 #define PI 3.14159265
 
@@ -19,14 +23,20 @@ double waveTable[256]; // a table for putting the waves
 for (int i = 0; i < 256; i++) {
 	double rad = (i * 2 * PI) / 256;
 	waveTable[i] = (sin(rad) + 1) * 128;
-	printf("%lf\n", waveTable[i]);
 }
 
 
 printf("Please enter a frequency: ");
 scanf("%d", &n);
 printf("You entered: %d\n",n);
+long delayTime = 1000/n;
 
+while(1) {
+	for (int i = 0; i < 256; i++) {
+		printf("%lf\n", waveTable[i]);
+		delay(delayTime); 
+	}
+}
 
 return 0;
 }
