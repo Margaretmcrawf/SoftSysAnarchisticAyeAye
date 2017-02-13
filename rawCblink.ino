fded7a20d641090c54619d90a45ad7feb96bcff2
JosephLee19 <leejos17@gmail.com>
2017-02-13 12:28:20 -0500

    Arduino raw c for square wave production on our hardware

---
+++ rawCblink.ino
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
+ 
+#define ON_TIME 5
+#define OFF_TIME 5
+ 
+int main (void)
+{
+ DDRA = 0xff;
+
+ while(1) {
+  PORTA = 0xff;
+
+  _delay_ms(ON_TIME);
+ 
+  PORTA &= 0x00;
+  _delay_ms(OFF_TIME);
+ }
+}
